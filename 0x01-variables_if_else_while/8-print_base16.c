8164401aed9450df8b3afc4204b8fbf15fe725d7
Deevinee <favour.divine.t@gmail.com>
2023-03-17 15:35:01 -0700

    var8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int num;
 char la;
 for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
-for (la = 'a'; ld <= 'f'; ld++)
+for (la = 'a'; la <= 'f'; la++)
 putchar(la);
 putchar('\n');
 return (0);
