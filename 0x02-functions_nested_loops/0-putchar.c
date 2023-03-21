ff295c75e07ee06a73fb59c4b68be17fdbf7203b
Deevinee <favour.divine.t@gmail.com>
2023-03-21 15:56:08 -0700

    loop0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints _putchar in a new line
+ * Return: 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchat('r');
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c: the character to print 
+ * Return: on success 1
+ */
+int _putchar(char)
+{
+return (write(1, &c, 1));
+}
