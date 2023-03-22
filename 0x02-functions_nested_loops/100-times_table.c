46b13cf3c6b2c27c552a82d768432499d9cd7372
Deevinee <favour.divine.t@gmail.com>
2023-03-22 05:42:20 -0700

    loop12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
  */
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int num, mult, prod;	
 
 	if (n >= 0 && n <= 15)
 	{
@@ -22,20 +22,18 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
+				if (prod >= 100)
 				{
-					if (prod >= 100)
-					{
-						_putchar((prod / 100) + '0');
-						_putchar(((prod / 10)) % 10 + '0');
-					}
-					else if (prod <= 99 && prod >= 10)
-					{
-						_putchar((prod / 10) + '0');
-					}
-					_putchar((prod % 10) + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				_putchar('\n');
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
