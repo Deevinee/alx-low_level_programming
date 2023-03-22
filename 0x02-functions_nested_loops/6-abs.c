4952ef45a4241bc78b9f2f9e5e1c3d40814e3ef1
Deevinee <favour.divine.t@gmail.com>
2023-03-21 17:23:36 -0700

    loop6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - compute the absloute value of an integer
+ * @t: the integer to be computed
+ * Return: the absloute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
