d964bd164320141fcce1c6f9697a2069df41709b
Deevinee <favour.divine.t@gmail.com>
2023-03-29 18:13:47 -0700

    morep7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,13 +9,13 @@ char *leet(char *n)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
-	CHAR S2[] = "4433007711";
+	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s2[j])
+			if (n[i] == s1[j])
 			{
 				n[i] = s2[j];
 			}
