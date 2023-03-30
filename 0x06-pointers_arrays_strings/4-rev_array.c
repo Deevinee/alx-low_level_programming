382688351fae854aed9efc671cafe9b22cb24e82
Deevinee <favour.divine.t@gmail.com>
2023-03-29 17:12:47 -0700

    morep4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _rev_array - reverses an array of integers
+ * rev_array - reverses an array of integers
  * @a: array
- * @n: numbers of elememts of thr array
+ * @n: number of elements of the array
  * Return: rev
  */
 void reverse_array(int *a, int n)
